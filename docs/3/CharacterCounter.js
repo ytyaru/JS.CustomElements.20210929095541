8593e924a2706dd52b8081d4ef8d4dc3022fcaa1
ytyaru <ytyaru@outlook.jp>
2021-10-01 09:55:56 +0900

    リファクタリング（不要引数削除。再計算回数の最小化）

--- docs/3/CharacterCounter.js
+++ docs/3/CharacterCounter.js
@@ -6,25 +6,27 @@ export default class CharacterCounter extends HTMLElement {
         this._withWhiteSpace = false;
         this._count = this.count(this._selector);
     }
-    count(selector='body') {
-        let target = document.querySelector(selector);
+    count() {
+        let target = document.querySelector(this._selector);
         let str = target.textContent
         if (!this._withWhiteSpace) { str = str.replace(/\s+/g, ""); console.log(`${str.length} this._withWhiteSpace=${this._withWhiteSpace}`); }
         return str.length;
     }
     static get observedAttributes() { return ['selector', 'unit', 'space']; }
     attributeChangedCallback(name, oldValue, newValue) {
+        let isRecount = false;
         if ("selector" == name) {
             this._selector = newValue;
-            this._count = this.count(this._selector);
+            isRecount = true;
         }
         if ("unit" == name) { this._unit = newValue; }
         if ("space" == name) {
             this._withWhiteSpace = true;
             const falseValues = ['0', 'off', 'false'];
             if (falseValues.includes(newValue)) { this._withWhiteSpace = false; }
-            this._count = this.count(this._selector);
+            isRecount = true;
         }
+        if (isRecount) { this._count = this.count(this._selector); }
         this._updateRendering();
     }
     get unit() { return this._unit; }
